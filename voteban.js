1db08147811ae516f324cc508333a482effa94d5
TheKIted <49844576+TheKIted@users.noreply.github.com>
2019-04-21 16:41:43 -0300

    Update voteban.js

--- voteban.js
+++ voteban.js
@@ -1,29 +1,29 @@
 const Discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
+exports.run = async (bot, message, args) => {
   message.delete()
   let mensagemreturn3 = new Discord.RichEmbed()
-  .setColor("AQUA")
+  .setColor("0x36393e")
   .addField("Duff CommandError ", `${message.author} Oopss | **Você não tem permissão para banir bobinho(a) 😊**`)
   if (!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send(mensagemreturn3).then(msg => msg.delete(10000))
   var usuario = message.mentions.members.first() || message.guild.members.get(args[0]);
   let mensagemreturn = new Discord.RichEmbed()
-  .setColor("AQUA")
-  .addField("Duff CommandError ", `${message.author} Oopss | **Use:\n\n/ban <exemplo#0186>**`)
+  .setColor("0x36393e")
+  .addField("Duff CommandError ", `${message.author} Oopss | **Use:\n\nd.ban <exemplo#0186>**`)
   if (!usuario) return message.channel.send(mensagemreturn).then(msg => msg.delete(15000))
 
   let mensagemreturn2 = new Discord.RichEmbed()
-  .setColor("AQUA")
+  .setColor("0x36393e")
   .addField("Duff CommandError ", `${message.author} Oopss | **Você não pode se banir 😢**`)
   if (usuario == message.member) return message.channel.send(mensagemreturn2).then(msg => msg.delete(10000))
 
   let mensagemreturn4 = new Discord.RichEmbed()
-  .setColor("AQUA")
+  .setColor("0x36393e")
   .addField("Duff CommandError ", `${message.author} Oopss | **Não tenho permissões suficiente para banir este usuário!**`)
   if (!usuario.bannable) return message.channel.send(mensagemreturn4).then(msg => msg.delete(10000))
 
  
   const embed = new Discord.RichEmbed()
-  .setColor("AQUA")
+  .setColor("0x36393e")
   .setDescription(`${message.author}, você deseja banir ${usuario} - ${usuario.id}?`)
   message.channel.send (embed).then(msg => {
     msg.react("562377093820973096");
@@ -41,13 +41,13 @@ module.exports.run = async (bot, message, args) => {
       usuario.ban();
 
       const embed4 = new Discord.RichEmbed()
-      .setColor("AQUA")
+      .setColor("0x36393e")
       .setThumbnail(usuario.displayAvatarURL)
-      .setDescription(`**O banimento deste usuário foi aplicado com sucesso!**`)
-      message.channel.send(embed4).then(msg => msg.delete(500))
+      .setDescription(`**O banimento do <@${usuario.id}> foi aplicado com sucesso!**`)
+      message.channel.send(embed4).then(msg => msg.delete(100))
 
       const embed2 = new Discord.RichEmbed()
-      .setColor("AQUA")
+      .setColor("0x36393e")
       .setThumbnail(usuario.displayAvatarURL)
       .addField("🚫 BANIMENTO", `**<@${usuario.id}>** foi **BANIDO(A)**\n**Banido por:** ${message.author}`)
       message.channel.send(embed2)
@@ -58,7 +58,7 @@ module.exports.run = async (bot, message, args) => {
       oi.remove(message.author.id);
 
       const embed4 = new Discord.RichEmbed()
-      .setColor("AQUA")
+      .setColor("0x36393e")
       .setThumbnail(usuario.displayAvatarURL)
       .setDescription(`**O banimento deste usuário foi cancelado com sucesso!**`)
       message.channel.send(embed4).then(msg => msg.delete(5000))
@@ -68,7 +68,7 @@ module.exports.run = async (bot, message, args) => {
   });
 
 }
-module.exports.help = {
+exports.help = {
   name: "ban",
   aliases: ['banir']
 }
